8b63dfa51ed72347b776e52b7801dfd114dd6062
Jakob <Jakobah37@gmail.com>
2022-02-23 15:20:57 +0100

    Added: Creating interfaces for abstraction.

---
+++ src/main/java/dk/qitsuk/otunes/dataaccess/dataaccessobjects/RestRepository.java
@@ -0,0 +1,15 @@
+package dk.qitsuk.otunes.dataaccess.dataaccessobjects;
+
+import java.util.Collection;
+
+//T = Types of elements in the collection.
+public interface RestRepository <T> {
+    Collection<T> getAllCustomer();
+    T getCustomerById(int Id);
+    T getCustomerByName(String firstName, String lastName);
+
+
+
+
+
+}
